cd0c94cddede5aba2df2b765913d98de7fab2b38
splatchoot <95099041+splatchoot@users.noreply.github.com>
2024-04-04 14:22:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/splatchoot/fix-restart-steam/main/restart_steam.sh -P $HOME
 chmod +x $HOME/restart_steam.sh
+echo "Your Steam is located in your Home directory under the name restart_steam.sh, pin it to your taskbar or put it somewhere quickly accesible"
