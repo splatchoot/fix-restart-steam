fad2dacdacdb2a46aee7e9ebef8c256a656a2a64
splatchoot <95099041+splatchoot@users.noreply.github.com>
2024-04-04 14:20:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/splatchoot/fix-restart-steam/main/restart%20steam.sh -P $HOME
-chmod +x "$HOME/restart steam.sh"
+wget https://raw.githubusercontent.com/splatchoot/fix-restart-steam/main/restart_steam.sh -P $HOME
+chmod +x $HOME/restart_steam.sh
